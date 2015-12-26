ef60c1c7d73be4a3a56f4c74e93fdb4b026ce724
Krishna Chittabathini <kriskishore@yahoo.com>
2015-12-25 20:06:35 -0800

    Initial Commit

---
+++ selectlogic/src/main/java/com/selectlogic/SelectlogicApplication.java
@@ -0,0 +1,12 @@
+package com.selectlogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectlogicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelectlogicApplication.class, args);
+    }
+}
